f735a3c3efdb156b34bed5690b26b7bcfcd7a7f0
Izaias Lima <izaiaslima356@gmail.com>
2024-07-02 14:51:24 -0300

    feat: set up react hook form

--- src/layouts/default-layout.tsx
+++ src/layouts/default-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export function DefaultLayout() {
 	return (
-		<div className="bg-base-800 w-full h-screen flex items-center justify-center">
+		<div className="bg-base-800 w-full h-screen flex items-center justify-center px-4">
 			<div className="bg-base-700 text-white w-[1200px] rounded-md p-12">
 				<Header />
 				<Outlet />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,36 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Play } from "phosphor-react";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const newCycleFormValidationSchema = z.object({
+	task: z.string().min(1, "Informe a tarefa"),
+	minutesAmount: z.number().min(5).max(60),
+});
+
+type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
-	const [task, setTask] = useState("");
+	const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+		resolver: zodResolver(newCycleFormValidationSchema),
+		defaultValues: {
+			task: "",
+			minutesAmount: 0,
+		},
+	});
+	const task = watch("task");
+	const isSubmitButtonDisabled = !task;
+
+	function handleCreateNewCycle(data: any) {
+		console.log(data);
+	}
 
 	return (
 		<div className="flex flex-col flex-1 items-center justify-center">
-			<form className="flex flex-col items-center gap-14">
+			<form
+				onSubmit={handleSubmit(handleCreateNewCycle)}
+				className="flex flex-col items-center gap-14"
+			>
 				<div
 					className="w-full flex items-center justify-center gap-2 text-base-100 text-lg
 				font-bold flex-wrap"
@@ -20,6 +44,7 @@ export function Home() {
 						text-base-100 flex-1 placeholder:shadow-none focus:border-emerald-500 focus:outline-none
 						appearance-none"
 						placeholder="Nome do projeto"
+						{...register("task")}
 					/>
 
 					<datalist id="task-suggestions">
@@ -31,14 +56,15 @@ export function Home() {
 
 					<label htmlFor="minutesAmount">durante</label>
 					<input
-						type="number"
 						id="minutesAmount"
+						type="number"
 						className="bg-transparent h-10 border-0 border-base-500 border-b-2 font-bold text-lg
 						text-base-100 flex-1 w-16 placeholder:shadow-none focus:border-emerald-500 focus:outline-none"
 						placeholder="00"
 						step={5}
 						min={5}
 						max={60}
+						{...register("minutesAmount", { valueAsNumber: true })}
 					/>
 
 					<span>minutos.</span>
@@ -56,8 +82,10 @@ export function Home() {
 
 				<button
 					type="submit"
+					disabled={isSubmitButtonDisabled}
 					className="bg-product-500 w-full flex items-center gap-2 justify-center p-4 rounded-lg
-					font-bold cursor-pointer text-base-100 hover:bg-product-600 transition-colors"
+					font-bold cursor-pointer text-base-100 hover:bg-product-600 transition-colors
+					disabled:bg-product-700 disabled:cursor-not-allowed"
 				>
 					<Play size={24} />
 					Começar
